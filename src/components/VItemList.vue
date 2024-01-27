168cdf2ea233a1ca4c3caf25ced206586f4b2301
nhoda <nicohodali@gmail.com>
2024-01-27 13:52:08 -0300

    feature: sorted items in list

--- src/components/VItemList.vue
+++ src/components/VItemList.vue
@@ -1,7 +1,8 @@
 <script setup>
+import { computed, ref } from 'vue';
 import VItem from './VItem.vue';
 
-defineProps({
+const props = defineProps({
   items: Array
 })
 
@@ -10,16 +11,33 @@ const emit = defineEmits([
   'onDeleteItem',
   'onToggleItem'
 ])
+
+const sortValue = ref('input')
+const sortedItems = computed(() => {
+  if (sortValue.value === 'description') {
+    return [...props.items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  }
+
+  if (sortValue.value === 'packed') {
+    return [...props.items].sort((a, b) =>
+      Number(a.packed) - Number(b.packed)
+    );
+  }
+
+  return props.items
+})
 </script>
 
 <template>
   <div class="list">
     <ul>
-      <VItem v-for="item in items" :key="item.id" :item="item" @on-toggle-item="emit('onToggleItem', item.id)"
+      <VItem v-for="item in sortedItems" :key="item.id" :item="item" @on-toggle-item="emit('onToggleItem', item.id)"
         @on-delete-item="emit('onDeleteItem', item.id)" />
     </ul>
     <div class="actions">
-      <select>
+      <select v-model="sortValue">
         <option value="input">Sort by input order</option>
         <option value="description">Sort by description</option>
         <option value="packed">Sort by packed status</option>
